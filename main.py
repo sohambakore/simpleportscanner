7a77159788c00f96bd62c98361e38eced821158d
Soham Bakore <33146295+sohambakore@users.noreply.github.com>
2019-09-18 14:24:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ print ('Simple Port Scanner**work in progress**')
 import socket
 ip = raw_input("Enter IP address: ")
 port input("Enter Port: ")
+#TCP socket, if it were to be a UDP socket it would be socket.SOCK_DGRAM
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if sock_connect_ex((ip,port)):
